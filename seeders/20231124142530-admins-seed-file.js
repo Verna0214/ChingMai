3bb6b7b1eb10cb87f2f9a46cca47d80b7fe2635a
Verna <yuhsichen0214@gmail.com>
2023-11-24 22:31:12 +0800

    feat: add admin table and seed

---
+++ seeders/20231124142530-admins-seed-file.js
@@ -0,0 +1,34 @@
+'use strict'
+const bcrypt = require('bcryptjs')
+
+/** @type {import('sequelize-cli').Migration} */
+module.exports = {
+  async up (queryInterface, Sequelize) {
+    /**
+     * Add seed commands here.
+     *
+     * Example:
+     * await queryInterface.bulkInsert('People', [{
+     *   name: 'John Doe',
+     *   isBetaMember: false
+     * }], {});
+    */
+    await queryInterface.bulkInsert('Admins', [{
+      name: 'root',
+      email: 'root@example',
+      password: bcrypt.hashSync('12345678', 10),
+      created_at: new Date(),
+      updated_at: new Date()
+    }], {})
+  },
+
+  async down (queryInterface, Sequelize) {
+    /**
+     * Add commands to revert seed here.
+     *
+     * Example:
+     * await queryInterface.bulkDelete('People', null, {});
+     */
+    await queryInterface.bulkDelete('Admins', null, {})
+  }
+}
