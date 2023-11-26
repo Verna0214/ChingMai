cf095f076d0029fdc08aed8d38240cd523a04732
Verna <yuhsichen0214@gmail.com>
2023-11-26 12:15:19 +0800

    feat: add categories seeds

--- seeders/20231126013328-spots-seed-file.js
+++ seeders/20231126013328-spots-seed-file.js
@@ -4,6 +4,10 @@ const faker = require('faker')
 /** @type {import('sequelize-cli').Migration} */
 module.exports = {
   async up (queryInterface, Sequelize) {
+    const categories = await queryInterface.sequelize.query(
+      'SELECT id FROM Categories;',
+      { type: queryInterface.sequelize.QueryTypes.SELECT }
+    )
     await queryInterface.bulkInsert('Spots',
       Array.from({ length: 50 }, () => ({
         name: faker.name.findName(),
@@ -13,7 +17,8 @@ module.exports = {
         closed_hours: '22:00',
         description: faker.lorem.paragraph(),
         created_at: new Date(),
-        updated_at: new Date()
+        updated_at: new Date(),
+        category_id: categories[Math.floor(Math.random() * categories.length)].id
       })), {})
   },
 
