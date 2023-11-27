c6dba3fe65e069de63f29d2f81351009fe2e439c
Verna <yuhsichen0214@gmail.com>
2023-11-27 10:30:26 +0800

    feat: modify text style

--- routes/modules/admin.js
+++ routes/modules/admin.js
@@ -4,10 +4,10 @@ const router = express.Router()
 const adminController = require('../../controllers/admin-controller')
 
 router.get('/spots/:id/edit', adminController.editSpotPage)
+router.get('/spots/create', adminController.createSpotPage)
 router.get('/spots/:id', adminController.getSpot)
 router.put('/spots/:id', adminController.putSpot)
 router.delete('/spots/:id', adminController.deleteSpot)
-router.get('/spots/create', adminController.createSpotPage)
 router.get('/spots', adminController.getSpotsPage)
 router.post('/spots', adminController.postSpot)
 
