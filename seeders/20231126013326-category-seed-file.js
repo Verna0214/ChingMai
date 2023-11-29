5bb35d53e907f92665f5ef59502a5f8bb76543ee
Verna <yuhsichen0214@gmail.com>
2023-11-29 10:05:22 +0800

    feat: add image column for categories table

--- seeders/20231126013326-category-seed-file.js
+++ seeders/20231126013326-category-seed-file.js
@@ -4,11 +4,12 @@
 module.exports = {
   async up (queryInterface, Sequelize) {
     await queryInterface.bulkInsert('Categories',
-      ['咖啡廳', '購物', '美食', '文化'].map(item => {
+      ['咖啡廳', '購物', '美食', '文化', '按摩'].map(item => {
         return {
           name: item,
           created_at: new Date(),
-          updated_at: new Date()
+          updated_at: new Date(),
+          image: `https://loremflickr.com/300/220/restaurant,food/?random=${Math.random() * 100}`
         }
       }), {})
   },
