3f7d5575029beee316d1e7d7898f41270ecda1f7
Verna <yuhsichen0214@gmail.com>
2023-12-04 22:08:08 +0800

    feat: add config.js

--- app.js
+++ app.js
@@ -25,7 +25,7 @@ app.use('/upload', express.static(path.join(__dirname, 'upload')))
 app.use(session({
   secret: process.env.SESSION_SECRET,
   resave: false,
-  saveUninitialized: false
+  saveUninitialized: true
 }))
 app.use(passport.initialize())
 app.use(passport.session())
---
+++ config/config.js
@@ -0,0 +1,24 @@
+module.exports = {
+  development: {
+    username: 'root',
+    password: '214a1990',
+    database: 'chingmai',
+    host: '127.0.0.1',
+    dialect: 'mysql'
+  },
+  test: {
+    username: 'root',
+    password: null,
+    database: 'database_test',
+    host: '127.0.0.1',
+    dialect: 'mysql'
+  },
+  production: {
+    host: process.env.RDS_HOSTNAME,
+    username: process.env.RDS_USERNAME,
+    password: process.env.RDS_PASSWORD,
+    port: process.env.RDS_PORT,
+    database: process.env.RDS_DB_NAME,
+    dialect: 'mysql'
+  }
+}
